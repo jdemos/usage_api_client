774724d55db780647fbf93f6b6287980ba71553c
John D <jdemos@twitter.com>
2016-03-30 15:13:15 -0600

    JD: Add config/options yaml file and opt parser code.

--- usage_api_ruby_app.rb
+++ usage_api_ruby_app.rb
@@ -1,8 +1,26 @@
+require 'optparse'
 
 
 if __FILE__ ==$0 # This script code is executed when running this file.
 
-# -- WRITE DATA TO FILE
+# Option Parser
+options = {}
+
+parser = OptionParser.new do |opts|
+  opts.on('-b', '--bucket', 'Bucket') do |b|
+    options[:bucket] = b;
+  end
+  opts.on('-t', '--todate', 'toDate') do |t|
+    options[:todate] = t;
+  end
+  opts.on('-f', '--fromdate', 'fromDate') do |f|
+    options[:fromdate] = f;
+  end
+end
+
+parser.parse!
+
+# - WRITE DATA TO FILE
 
 # create timestamp for file naming
 time = Time.new
