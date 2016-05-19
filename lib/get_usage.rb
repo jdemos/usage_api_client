e34418061173ccb2215051e34bf9fcee9beafc27
John D <jdemos@twitter.com>
2016-05-19 13:16:46 -0600

    JD: Push current state to github.

---
+++ lib/get_usage.rb
@@ -0,0 +1,26 @@
+# API specific details
+require 'faraday'
+
+class GetUsage
+
+  # Define attributes of GetUsage
+  attr_accessor :options
+
+  # load yml file
+  def load_account_details(file_name)
+    @options = YAML.load_file(file_name)
+  end
+
+  def build_request()
+    # create variable to store the root URL
+    request = Faraday.new(url: "https://gnip-api.twitter.com")
+    request.basic_auth(@options['user_name'], @options['password'])
+    response = request.get "/metrics/usage/accounts/#{@options['account_name']}.json" # ?bucket=#{bucket}&fromdate=#{fromdate}&todate=#{todate}")
+
+  end
+end
+
+# get usage metrics()
+# print_summary()
+
+# get metrics by product (product_name)
--- usage_api_ruby_app.rb
+++ usage_api_ruby_app.rb
@@ -1,27 +1,48 @@
+require 'json'
 require 'optparse'
+require 'yaml'
 
+# Call class
+require_relative './lib/get_usage'
 
-if __FILE__ ==$0 # This script code is executed when running this file.
+if __FILE__ == $0 # This script code is executed when running this file.
 
-# Option Parser
-options = {}
+  # Option Parser
+  cli_options = {}
 
-parser = OptionParser.new do |opts|
-  opts.on('-b', '--bucket', 'Bucket') do |b|
-    options[:bucket] = b;
+  parser = OptionParser.new do |opts|
+    opts.on('-a', '--accountdetails', 'accountDetails - specify yml file path and name') do |a|
+      cli_options[:accountdetails] = a;
+    end
+    opts.on('-b', '--bucket', 'Bucket (month or day)') do |b|
+      cli_options[:bucket] = b;
+    end
+    opts.on('-t', '--todate', 'toDate - is exclusive and UTC time (YYYYMMDDHHMM)') do |t|
+      cli_options[:todate] = t;
+    end
+    opts.on('-f', '--fromdate', 'fromDate - is inclusive and UTC time (YYYYMMDDHHMM)') do |f|
+      cli_options[:fromdate] = f;
+    end
   end
-  opts.on('-t', '--todate', 'toDate') do |t|
-    options[:todate] = t;
+
+  parser.parse!
+
+  # Set default location of config accounts file
+  if cli_options[:accountdetails].nil?
+    cli_options[:accountdetails] = "config/accounts.yml"
   end
-  opts.on('-f', '--fromdate', 'fromDate') do |f|
-    options[:fromdate] = f;
-  end
+
+  client = GetUsage.new()
+    # Load cli options from accounts.yml
+    client.load_account_details(cli_options[:accountdetails])
+
+    response = client.build_request
+
+    puts response
 end
-
-parser.parse!
 
 # - WRITE DATA TO FILE
 
-# create timestamp for file naming
-time = Time.new
-timestamp = time.hour.to_s + "_" + time.min.to_s + "_" + time.sec.to_s
+# # create timestamp for file naming
+# time = Time.new
+# timestamp = time.hour.to_s + "_" + time.min.to_s + "_" + time.sec.to_s
